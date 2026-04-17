2eca61401d4dba7cc3813b94303129290ba2912c
lucas <ramosmanhaes@outlook.com>
2026-04-17 16:45:20 -0300

    chore: add betther auth table

--- db/index.ts
+++ db/index.ts
@@ -2,4 +2,13 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+import * as schema from "./schema" 
+
+export const db = drizzle(process.env.DATABASE_URL!, {
+    schema: { 
+        ...schema,
+        user: schema.userTable,
+        session: schema.sessionTable,
+        account: schema.sessionTable
+    }
+});
